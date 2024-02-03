89dbd77477ecc682022e8dda95a148393df75242
lea330 <88422103+lea330@users.noreply.github.com>
2024-02-03 15:27:17 +0900

    Add files via upload

--- bodyfriend/js/main.js
+++ bodyfriend/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	
+	//메인 배너
 	new Swiper('.swiper', {
 		
 		effect : 'fade',
@@ -16,14 +17,14 @@ $(document).ready(function(){
 	})
 	$('.mb_pause').on('click', function(){
 		swiper.autoplay.stop();
-	  });
-	  $('.mb_start').on('click', function(){
+	});
+	$('.mb_start').on('click', function(){
 		swiper.autoplay.start();
-	  });
+	});
 
 
-
-	  new Swiper(".swiper2", {
+	//con4 배너
+	new Swiper(".swiper2", {
 		slidesPerView: 3,  
 		slidesPerGroup: 3,
 		spaceBetween: 40,
@@ -35,9 +36,22 @@ $(document).ready(function(){
 			el : '.swiper-scrollbar',
 			draggable : true, // 스크롤바를 드래그해서 움직일수 있는지 여부
 		},
-	  });
+		breakpoints: {
+			// 화면의 넓이가 320px 이상일 때
+			320: {
+			  slidesPerView: 1,
+			  slidesPerGroup: 1
+			},
+			// 화면의 넓이가 640px 이상일 때
+			640: {
+			  slidesPerView: 3
+			}
+		  }
 
-	  new Swiper(".swiper3", {
+	});
+
+	//con5 배너
+	new Swiper(".swiper3", {
 		effect : 'fade',
 		autoplay: {
 			// 자동재생 여부
@@ -49,7 +63,25 @@ $(document).ready(function(){
 			prevEl: '.con5_prev',
 			nextEl: '.con5_next',
 		  }
+	});
+
+	//모바일 메뉴 버튼 이벤트
+	$('.all').on('click',function(){
+		$(this).toggleClass('on');
+		$('.hd').toggleClass('on');
+	})
+
+	//스크롤 시 메뉴 배경색 변경
+	$(window).scroll(function(){
+		if($(document).scrollTop() > 0) {
+		  $('.menu').addClass('active');
+		} else {
+		  $('.menu').removeClass('active');
+		}
 	  })
-	  
+	
+
 });
+
+
 
